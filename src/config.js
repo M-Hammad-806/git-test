2ce8a571bca784fd4140c27f9f353002760f5629
Muhammad Hammad <806mhk@gmail.com>
2025-02-07 18:56:38 +0500

    version 12

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-console.log("version 2");
+console.log("version 12");
 console.log("new thing added in version 2");
 console.log("project Config files");
--- src/main.js
+++ src/main.js
@@ -1,2 +1,2 @@
-console.log("Main file version 2");
+console.log("Main file version 12");
 console.log("application main file");
